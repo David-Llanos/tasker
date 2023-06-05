65bf2d8f9c1b0c78284e5213616943402015d729
david-llanos <dllanosx@gmail.com>
2023-06-05 18:37:33 -0400

    refresh button implemented to poll the folder

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from dash import dcc, html, dash_table, Output, Input, State, MATCH
 import dash_bootstrap_components as dbc
 import math
 
-
-
 # initialize Dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -22,7 +20,7 @@ color_list = [
     "#00FF00", # Lime
     "#00FFFF", # Aqua
     "#FF00FF", # Fuchsia
-    "#800000", # Maroon
+    # "#800000", # Maroon
     "#008000", # Green
     "#808000", # Olive
     "#800080", # Purple
@@ -37,41 +35,47 @@ color_list = [
     "#FFA07A"  # LightSalmon
 ]
 
-color_counter = 0
-children = []
-for i, file in enumerate(csv_files):
-    df = pd.read_csv(os.path.join(directory, file))
-    table = dash_table.DataTable(
-        id={'type': 'dynamic-table', 'index': file}, 
-        columns=[{"name": c, "id": c} for c in df.columns],
-        data=df.to_dict('records'),
-        page_size=2,
-        page_current= 0, 
-        editable=True,
-        filter_action="native" ,
-        style_header={'backgroundColor': color_list[color_counter]}, # Apply color to table
-    )
-    last_page_button = html.Button('Last Page', 
-                                   id={'type': 'dynamic-last-page-button', 'index': file}, 
-                                   style={'backgroundColor': color_list[color_counter]})  # Apply color to button
-    add_button = html.Button('Add Row', 
-                             id={'type': 'dynamic-add-button', 'index': file}, 
-                             style={'backgroundColor': color_list[color_counter]})  # Apply color to button
-    save_button = html.Button('Save', 
-                              id={'type': 'dynamic-save-button', 'index': file}, 
-                              style={'backgroundColor': color_list[color_counter]})  # Apply color to button
-    children.extend([html.H2(file), table, last_page_button, add_button, save_button, html.Hr()])  # Added the last_page_button here
+# Define the refresh function
+def refresh():
+    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+    print(csv_files)
 
-    # Increment the color counter
-    color_counter += 1
+    color_counter = 0
+    children = []
+    for i, file in enumerate(csv_files):
+        df = pd.read_csv(os.path.join(directory, file))
 
-    # If we've gone through all the colors, reset the counter
-    if color_counter == len(color_list):
-        color_counter = 0
+        table = dash_table.DataTable(
+            id={'type': 'dynamic-table', 'index': file}, 
+            columns=[{"name": c, "id": c} for c in df.columns],
+            data=df.to_dict('records'),
+            page_size=5,
+            page_current= 0, 
+            editable=True,
+            filter_action="native" ,
+            style_header={'backgroundColor': color_list[color_counter]}, # Apply color to table
+            )
+        last_page_button = html.Button('Last Page', 
+                                    id={'type': 'dynamic-last-page-button', 'index': file}, 
+                                    style={'backgroundColor': color_list[color_counter]})  # Apply color to button
+        add_button = html.Button('Add Row', 
+                                id={'type': 'dynamic-add-button', 'index': file}, 
+                                style={'backgroundColor': color_list[color_counter]})  # Apply color to button
+        save_button = html.Button('Save', 
+                                id={'type': 'dynamic-save-button', 'index': file}, 
+                                style={'backgroundColor': color_list[color_counter]})  # Apply color to button
+        children.extend([html.H2(file), table, last_page_button, add_button, save_button, html.Hr()])
+
+        # increment color_counter and use modulo to keep it within the length of color_list
+        color_counter = (color_counter + 1) % len(color_list)
+
+    return children
 
 
-
-app.layout = html.Div(children=children)
+app.layout = html.Div([
+    dbc.Button('Refresh', id='refresh-button', color='primary'),
+    html.Div(id='tables-container')
+])
 
 @app.callback(
     Output({'type': 'dynamic-table', 'index': MATCH}, 'data'),
@@ -82,7 +86,6 @@ def add_row(n_clicks, rows, columns):
     if n_clicks is not None:
         rows.append({c['id']: '' for c in columns})
     return rows
-
 
 @app.callback(
     Output({'type': 'dynamic-save-button', 'index': MATCH}, 'children'),
@@ -97,23 +100,23 @@ def save_changes(n_clicks, rows, filename):  # added filename argument
         print(f"{file_path} saved !")
     return 'Save'
 
-
-
-# The callback for going to last page
-
 @app.callback(
     Output({'type': 'dynamic-table', 'index': MATCH}, 'page_current'),
     Input({'type': 'dynamic-last-page-button', 'index': MATCH}, 'n_clicks'),
     State({'type': 'dynamic-table', 'index': MATCH}, 'data'),
-    State({'type': 'dynamic-table', 'index': MATCH}, 'page_current'),
-    prevent_initial_call=True)
+    State({'type': 'dynamic-table', 'index': MATCH}, 'page_current'))
 def go_to_last_page(n_clicks, rows, current_page):
     if n_clicks is not None:
         last_page = (len(rows) - 1) // 2  # Updated formula
         return last_page
     return dash.no_update
 
-
+@app.callback(
+    Output('tables-container', 'children'),
+    [Input('refresh-button', 'n_clicks')]
+)
+def handle_refresh(n_clicks):
+    return refresh()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
